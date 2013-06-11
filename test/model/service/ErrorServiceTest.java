5ca5b671105c2ae157b8b425ff7d9d8c9485c6ac
Sergio Andrés Cortés Hernández <sacortesh@gmail.com>
2013-06-10 23:00:38 -0500

    Terminadas las pruebas para el servicio de errores

--- test/model/service/ErrorServiceTest.java
+++ test/model/service/ErrorServiceTest.java
@@ -5,6 +5,10 @@
 package model.service;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.dao.exceptions.NonexistentEntityException;
+import model.dao.exceptions.PreexistingEntityException;
 import model.vo.ErrorVO;
 import org.junit.*;
 import static org.junit.Assert.*;
@@ -14,7 +18,9 @@ import static org.junit.Assert.*;
  * @author Zergio
  */
 public class ErrorServiceTest {
-    
+
+    ErrorVO error;
+
     public ErrorServiceTest() {
     }
 
@@ -25,40 +31,70 @@ public class ErrorServiceTest {
     @AfterClass
     public static void tearDownClass() throws Exception {
     }
-    
+
     @Before
     public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+
+        error = new ErrorVO();
+
+        error.setError("Descripción error");
+        error.setUsuariosDNI(new Long("0"));
+        error.setInterfaz("Interfaz de incidencia");
+        error.setId(1);
+
+
     }
 
-   @Test
-    public void testCreateErrorExpectedFunctionality(){
-        
+    @After
+    public void tearDown() {
+
+        error = null;
+
     }
-    
+
     @Test
-    public void testFindErrorExpectedFunctionality(){
-        
+    public void testCreateErrorExpectedFunctionality() {
+
+        try {
+            ServiceFactory.getInstance().getErrorService().create(error);
+        } catch (Exception ex) {
+            Logger.getLogger(ErrorServiceTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail();
+        }
+
     }
-    
-        
+
     @Test
-    public void testUpdateErrorExpectedFunctionality(){
-        
+    public void testFindErrorExpectedFunctionality() {
+
+        assertEquals(ServiceFactory.getInstance().getErrorService().find(error.getId()), error);
+
     }
-    
+
     @Test
-    public void testDeleteErrorExpectedFunctionality(){
-        
+    public void testUpdateErrorExpectedFunctionality() {
+
+        ErrorVO tempError = ServiceFactory.getInstance().getErrorService().find(error.getId());
+        tempError.setError("Cambio en error");
+        try {
+            ServiceFactory.getInstance().getErrorService().update(tempError);
+        } catch (Exception ex) {
+            Logger.getLogger(ErrorServiceTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail();
+        }
+
+        assertEquals(ServiceFactory.getInstance().getErrorService().find(error.getId()).getError(), "Cambio en error");
+
+
     }
-    
+
     @Test
-    public void testRemoveAllErrorExpectedFunctionality(){
-        
+    public void testDeleteErrorExpectedFunctionality() {
+        try {
+            ServiceFactory.getInstance().getErrorService().delete(error.getId());
+        } catch (Exception ex) {
+            Logger.getLogger(ErrorServiceTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail();
+        }
     }
-    
-    
 }
